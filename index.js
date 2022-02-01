52d5859ff8ba13c5ab2c4d97e080de2352e15a5b
Shea Cronin <shealcronin@gmail.com>
2022-02-01 15:58:02 -0500

    Rewrite site using Express

--- index.js
+++ index.js
@@ -1,60 +1,9 @@
-const http = require('http');
 const path = require('path');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
-
-    // Initial content type
-    let contentType = 'text/html';
-
-    // Extension of file
-    let extname = path.extname(filePath);
-    if (extname === '') {
-        filePath += '.html';
-    }
-
-    // Check ext and set content type
-    switch(extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-    }
-
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                // Page not found
-                fs.readFile(path.join(__dirname, '404.html'), (err, content) => {
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.end(content, 'utf8');
-                });
-            } else {
-                // Some server error
-                res.writeHead(500);
-                res.end(`Server error: ${err.code}`);
-            }
-        } else {
-            // Success
-            res.writeHead(200, {'Content-Type': contentType });
-            res.end(content, 'utf8');
-        }
-    });
-});
+app.use(express.static(path.join(__dirname, 'public'), {extensions: ['html']}));
 
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
